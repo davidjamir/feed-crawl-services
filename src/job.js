05699b7f4be4105ca35acf19a29a83b24acd4714
Hoa Duc Viet <hoaducviet@Viet-MBP.local>
2026-02-16 02:07:11 +0700

    Change form for convert json feed

--- src/job.js
+++ src/job.js
@@ -65,7 +65,7 @@ function convertJsonFeedToRSSLike(data) {
       title: item.title,
       link: item.link,
       guid: item.guid,
-      creator: item.author || item.author.name,
+      creator: item?.author?.name || item?.author,
       categories: item.categories || [],
       pubDate: item.pubDate || item.date_published,
       summary: item.summary,
